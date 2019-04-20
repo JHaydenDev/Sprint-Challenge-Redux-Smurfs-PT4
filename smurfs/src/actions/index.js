d5226430d2d10f9dba6386b9290f63c6378e70c6
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-04-19 21:26:01 -0500

    created the get request, add, and update actions.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,7 +2,15 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from "axios";
 
+export const ADDED = "ADDED";
+export const ADDING = "ADDING";
+export const FETCHING = "FETCHING";
+export const FETCHED = "FETCHED";
+export const UPDATED = "UPDATE";
+export const UPDATING = "UPDATING";
+export const ERROR = "ERROR";
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -13,3 +21,34 @@
    U - updateSmurf
    D - deleteSmurf
 */
+const host = `http://localhost:3333/smurfs`;
+
+export const getSmurfs = () => dispatch => {
+  const smurfs = axios.get(`${host}`);
+  dispatch({ type: FETCHING });
+  smurfs
+    .then(res => {
+      dispatch({ type: FETCHED, payload: res.data });
+    })
+    .catch(err => dispatch({ type: ERROR, payload: err }));
+};
+
+export const addSmurf = smurf => dispatch => {
+  const newGuy = axios.post(`${host}`, smurf);
+  dispatch({ type: ADDING });
+  newGuy
+    .then(res => {
+      dispatch({ type: ADDED, payload: res.data });
+    })
+    .catch(err => dispatch({ type: ERROR, payload: err }));
+};
+
+export const updateSmurf = (info, id) => dispatch => {
+  const updatedSmurf = axios.put(`${host}/${id}`, info);
+  dispatch({ type: UPDATING });
+  updatedSmurf
+    .then(res => {
+      dispatch({ type: UPDATED, payload: res.data });
+    })
+    .catch(err => dispatch({ type: ERROR, payload: err }));
+};
